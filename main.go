3d76caaf5bd0122d275842b7a9e71853a9d62adc
Rafael G. Campi <rafael.g.campi@gmail.com>
2025-04-24 07:16:06 -0300

    feat: first commit

--- main.go
+++ main.go
@@ -6,17 +6,14 @@ import (
     "net/http"
 )
 
-// Função de handler para a rota "/"
 func home(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Api funcionando!")
 }
 
 
 func main() {
-    // Definindo as rotas
     http.HandleFunc("/", home)
 
-    // Inicializando o servidor na porta 8080
     fmt.Println("Iniciando o servidor na porta 8080...")
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
